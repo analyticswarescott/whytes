de036e6b67a37e68b8d5085c3307bf3d13ded8b6
rachoac <aron.racho@gmail.com>
2016-07-27 21:26:23 -0700

    cleanup, pared down case

--- xfilter_xplore/dataset_explorer_mockup.js
+++ xfilter_xplore/dataset_explorer_mockup.js
@@ -38,7 +38,9 @@ d3.csv('data/2014_SALES.csv', function (data) {
         if (d.m_sales != "NULL") {
             return d.m_sales;
         }
-        else  {return 0;}
+        else {
+            return 0;
+        }
 
     });
 
@@ -46,7 +48,9 @@ d3.csv('data/2014_SALES.csv', function (data) {
         if (d.m_sales != "NULL") {
             return d.m_sales;
         }
-        else  {return 0;}
+        else {
+            return 0;
+        }
 
     });
 
@@ -54,7 +58,9 @@ d3.csv('data/2014_SALES.csv', function (data) {
         if (d.m_sales != "NULL") {
             return d.m_sales;
         }
-        else  {return 0;}
+        else {
+            return 0;
+        }
 
     });
 
@@ -62,7 +68,9 @@ d3.csv('data/2014_SALES.csv', function (data) {
         if (d.m_sales != "NULL") {
             return d.m_sales;
         }
-        else  {return 0;}
+        else {
+            return 0;
+        }
 
     });
 
@@ -70,7 +78,9 @@ d3.csv('data/2014_SALES.csv', function (data) {
         if (d.m_sales != "NULL") {
             return d.m_sales;
         }
-        else  {return 0;}
+        else {
+            return 0;
+        }
 
     });
 
@@ -78,57 +88,67 @@ d3.csv('data/2014_SALES.csv', function (data) {
         if (d.m_gm != "NULL") {
             return d.m_sales * (1.1);
         }
-        else  {return 0;}
+        else {
+            return 0;
+        }
     });
 
     var m_rep_gm = dim_rep.group().reduceSum(function (d) {
         if (d.m_gm != "NULL") {
             return d.m_sales * 1.1;
         }
-        else  {return 0;}
+        else {
+            return 0;
+        }
     });
 
     var m_cust_gm = dim_customer.group().reduceSum(function (d) {
         if (d.m_gm != "NULL") {
             return d.m_sales * 1.1;
         }
-        else  {return 0;}
+        else {
+            return 0;
+        }
     });
 
     var m_cat_gm = dim_category.group().reduceSum(function (d) {
         if (d.m_gm != "NULL") {
             return d.m_sales * 1.1;
         }
-        else  {return 0;}
+        else {
+            return 0;
+        }
     });
 
     var m_reg_gm = dim_region.group().reduceSum(function (d) {
         if (d.m_gm != "NULL") {
             return d.m_sales * 1.1;
         }
-        else  {return 0;}
+        else {
+            return 0;
+        }
     });
 
-var test = m_month_sales.top(Infinity);
+    var test = m_month_sales.top(Infinity);
     var test2 = m_region_sales.top(Infinity);
     var test3 = m_customer_sales.top(Infinity);
-var grpCat = m_cat_sales.top(Infinity);
+    var grpCat = m_cat_sales.top(Infinity);
     var grpRep = m_rep_sales.top(Infinity);
 
 
-    var months = ["Jan","Feb","Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    c1 = createCompositeChart("#graph1", dim_month, m_month_sales, m_month_gm,  months, 600, 240, 30, 12, 1) ;
+    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    c1 = createCompositeChart("#graph1", dim_month, m_month_sales, m_month_gm, months, 600, 240, 30, 12, 1);
 
-     c2 = createCompositeChart("#graph2", dim_rep, m_rep_sales,  m_rep_gm, null, 726
-        , 240, 30, 15, 2) ;
+    c2 = createCompositeChart("#graph2", dim_rep, m_rep_sales, m_rep_gm, null, 726
+        , 240, 30, 15, 2);
 
-     c3 = createCompositeChart("#graph3", dim_customer, m_customer_sales,  m_cust_gm, null, 930
+    c3 = createCompositeChart("#graph3", dim_customer, m_customer_sales, m_cust_gm, null, 930
         , 240, 26.5, 22, 3);
 
-     c4 = createCompositeChart("#graph4", dim_category, m_cat_sales, m_cat_gm, null, 840
+    c4 = createCompositeChart("#graph4", dim_category, m_cat_sales, m_cat_gm, null, 840
         , 240, 29, 18, 4);
 
-     c5 = createCompositeChart("#graph5", dim_region, m_region_sales,  m_reg_gm, null, 410
+    c5 = createCompositeChart("#graph5", dim_region, m_region_sales, m_reg_gm, null, 410
         , 240, 30, 12, 5);
 
     createDataGrid("#test", dim_month, m_month_sales);
@@ -136,131 +156,132 @@ var grpCat = m_cat_sales.top(Infinity);
     dc.renderAll();
 
 
-
 });
 
-function redrawExcept( chartNo) {
-   // dc.renderAll();
+function redrawExcept(chartNo) {
+    // dc.renderAll();
 }
 
+function createCompositeChart(targetDiv, dimension, group1, group2, axisVals, w, h, gap, topn, chartNo) {
 
 
-    function createCompositeChart(targetDiv, dimension, group1, group2, axisVals, w, h, gap, topn, chartNo) {
-
-
-        var filteredGroup = (function (source_group) {return {
-            all:function () {
-                return source_group.top(topn).filter(function(d) {
-                    return d.key;;
+    var filteredGroup = (function (source_group) {
+        return {
+            all: function () {
+                return source_group.top(topn).filter(function (d) {
+                    return d.key;
+                    ;
                 });
             }
-        };})(group1);
+        };
+    })(group1);
 
 
-        //make sure second group follows first, not top 10 as it could be different
-        var filteredGroup2 = (function (source_group) {return {
-            all:function () {
-                return source_group.all().filter(function(d) {
-                    for (var i=0;  i<filteredGroup.all().length; i++) {
+    //make sure second group follows first, not top 10 as it could be different
+    var filteredGroup2 = (function (source_group) {
+        return {
+            all: function () {
+                return source_group.all().filter(function (d) {
+                    for (var i = 0; i < filteredGroup.all().length; i++) {
                         if (filteredGroup.all()[i].key === d.key) {
-                            return d.key;;
+                            return d.key;
+                            ;
                         }
                     }
 
                 });
             }
 
-        };})(group2);
+        };
+    })(group2);
 
 
+    var composite = dc.compositeChart(targetDiv);
 
-        var composite = dc.compositeChart(targetDiv) ;
+    /*        var vals;
+     if (!axisVals) {
+     vals = filteredGroup.all().map(function (d) {
+     return d.key; });
 
-/*        var vals;
-        if (!axisVals) {
-           vals = filteredGroup.all().map(function (d) {
-                return d.key; });
+     }
+     else {
+     vals = axisVals;
+     }*/
 
-        }
-        else {
-            vals = axisVals;
-        }*/
-
-        var clicked;
-        var c1 =  dc.barChart(composite)
-            //  .dimension(dim_month)
-            .gap(gap)
-            // .margins({left: 222})
-            .group(filteredGroup)
-            .colors('lightsteelblue')
-        .on('filtered', function(chart, filter){
+    var clicked;
+    var c1 = dc.barChart(composite)
+    //  .dimension(dim_month)
+        .gap(gap)
+        // .margins({left: 222})
+        .group(filteredGroup)
+        .colors('lightsteelblue')
+        .on('filtered', function (chart, filter) {
             redrawExcept(chartNo);
         });
 
 
-        var c2 =  dc.barChart(composite)
-            .gap(gap)
-            .centerBar(true)
-            .group(filteredGroup2)
-            .colors('green');
+    var c2 = dc.barChart(composite)
+        .gap(gap)
+        .centerBar(true)
+        .group(filteredGroup2)
+        .colors('green');
 
-        composite
-            .width(w)
-            .height(h)
-            ._rangeBandPadding(-0.5)
-            .dimension(dimension);
+    composite
+        .width(w)
+        .height(h)
+        ._rangeBandPadding(-0.5)
+        .dimension(dimension);
 
-            //months don't change for now to allow the ordering to work
-            if (!axisVals) {
-               composite.x(d3.scale.ordinal().domain(filteredGroup.all().map(function (d) {
-                   return d.key; })))
-            }
-            else {
-                composite.x(d3.scale.ordinal().domain(axisVals));
-            }
-
-
-            composite.compose([c1,c2])
-            .xUnits(dc.units.ordinal)
-            .mouseZoomable(true)
-            .on('postRedraw', function(chart){
-                clicked = null;
-
-            })
-            .elasticY(true)
-
-                ///.elasticX(true)
-            .brushOn(false);
-
-        composite.yAxis().tickFormat(function (d) {
-            var prefix = d3.formatPrefix(d);
-            return prefix.scale(d) + prefix.symbol;
-        });
-
-        composite.yAxis().tickSize(2);
-
-        composite.renderlet(function(chart) {
-            chart.selectAll("g._1").attr("transform", "translate(" + (22 + (gap - 30))  + ", 0)");
-            //d3.event.stopPropagation();
-        });
-
-        if (!axisVals) { //rotate non-month
-            composite.renderlet(function (chart) {
-                chart.selectAll("g.x text")
-                    .attr('dx', '-5')
-                    .attr('transform', "rotate(-15)");
-            });
-        }
-
-       return composite;
+    //months don't change for now to allow the ordering to work
+    if (!axisVals) {
+        composite.x(d3.scale.ordinal().domain(filteredGroup.all().map(function (d) {
+            return d.key;
+        })))
+    }
+    else {
+        composite.x(d3.scale.ordinal().domain(axisVals));
     }
 
-function createDataGrid(targetDiv, dimension, group)
-{
+    composite.compose([c1, c2])
+        .xUnits(dc.units.ordinal)
+        .mouseZoomable(true)
+        .on('postRedraw', function (chart) {
+            clicked = null;
+
+        })
+        .elasticY(true)
+
+        ///.elasticX(true)
+        .brushOn(false);
+
+    composite.yAxis().tickFormat(function (d) {
+        var prefix = d3.formatPrefix(d);
+        return prefix.scale(d) + prefix.symbol;
+    });
+
+    composite.yAxis().tickSize(2);
+
+    composite.renderlet(function (chart) {
+        chart.selectAll("g._1").attr("transform", "translate(" + (22 + (gap - 30)) + ", 0)");
+        //d3.event.stopPropagation();
+    });
+
+    if (!axisVals) { //rotate non-month
+        composite.renderlet(function (chart) {
+            chart.selectAll("g.x text")
+                .attr('dx', '-5')
+                .attr('transform', "rotate(-15)");
+        });
+    }
+
+    return composite;
+}
+
+function createDataGrid(targetDiv, dimension, group) {
 
     var grid = d3.divgrid();
 
-    d3.csv('data/month_pivot.csv', function(data) {
+    d3.csv('data/month_pivot.csv', function (data) {
         d3.select(targetDiv)
             .datum(data)
             .call(grid)
