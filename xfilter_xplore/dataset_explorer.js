b89ea500fa1e4282e13cd3e4bb49cee7cbe18bb6
rachoac <aron.racho@gmail.com>
2016-07-30 20:59:16 -0700

    making it possible to switch between datasets easily

--- xfilter_xplore/dataset_explorer.js
+++ xfilter_xplore/dataset_explorer.js
@@ -206,3 +206,16 @@ function cloneTemplate(gsX, gsY, gsWidth, gsHeight, title, id) {
 
     return template;
 }
+
+function queryParamValue(val) {
+    var result = "",
+        tmp = [];
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+            tmp = item.split("=");
+            if (tmp[0] === val) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
+}
---
+++ xfilter_xplore/readme.txt
@@ -0,0 +1,10 @@
+If you have python, run a server to serve the contents of this diretory:
+
+python -m SimpleHTTPServer 8000
+
+Then navigate to http://localhost:8000/demo.html.
+
+To view a different dataset, use the ?dataset parameter:
+
+http://localhost:8000/demo.html?dataset=whytes
+http://localhost:8000/demo.html?dataset=cordova
